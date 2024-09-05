cd4320c9edc5f7cb87ac060ef330e605c276e7a7
Pablo Prata <pabloalmeidaprata@hotmail.com>
2024-09-05 17:37:41 -0300

    Project initialized

---
+++ src/test/java/br/com/sages/catalog/bookstore_api/BookstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sages.catalog.bookstore_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
